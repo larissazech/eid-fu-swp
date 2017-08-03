fc7ba734d65644fb064ed1601a65bab0de5336f8
kelvin <kelvin.glass@fu-berlin.de>
2017-08-03 11:03:06 +0200

    Add more structured output to setup.sh
    * Add information which ip should appear in hosts file

--- setup.sh
+++ setup.sh
@@ -10,31 +10,39 @@ function addToEnv {
 }
 
 CERT_DIR=/etc/ssl/certs
+DOMAIN=eid.de
+IP4_PREFIX=$(hostname -I | cut -d' ' -f 1 | cut -d. -f 1,2)
 
-echo Remove existing setup files
+echo :Remove existing setup files
 rm .env {www,MAIN}.{cer,key} > /dev/null
 
-echo Shutdown the system if running
+echo :Shutdown the system if running
 sudo docker-compose down
 sudo docker volume rm eidfuswp_db_mysql
 
-echo Create .env file
-addToEnv "BOILERPLATE_DOMAIN=eid.de"
-addToEnv "BOILERPLATE_IPV4_16PREFIX=$(hostname -I | cut -d' ' -f 1 | cut -d. -f 1,2)"
+echo :Create .env file
+addToEnv "BOILERPLATE_DOMAIN=$DOMAIN"
+addToEnv "BOILERPLATE_IPV4_16PREFIX=$IP4_PREFIX"
 addToEnv "BOILERPLATE_IPV6_SUBNET=bade:affe:dead:beef:b011::/80"
 addToEnv "BOILERPLATE_IPV6_ADDRESS=bade:affe:dead:beef:b011:0642:ac10:0080"
 addToEnv "BOILERPLATE_WWW_CERTS=$CERT_DIR"
-addToEnv "BOILERPLATE_API_SECRETKEY=$(date | md5sum)"
+addToEnv "BOILERPLATE_API_SECRETKEY=$(date | md5sum | cut -d' ' -f 1)"
 sleep 1
-addToEnv "BOILERPLATE_DB_PASSWORD=$(date | md5sum)"
+addToEnv "BOILERPLATE_DB_PASSWORD=$(date | md5sum | cut -d' ' -f 1)"
+
+echo "#################### .env ####################"
+cat .env
+echo "##############################################"
+echo "Make sure the line '$IP4_PREFIX.0.128<tab>$DOMAIN' is in you /etc/hosts"
 
 
-echo Create ssl certifactes
+echo :Create ssl certifactes
 createCert MAIN
 createCert www
 
-sudo cp {MAIN,www}.{cer,key} "$CERT_DIR/"
+sudo mv {MAIN,www}.{cer,key} "$CERT_DIR/"
 sudo chown root:root $CERT_DIR/{MAIN,www}.{cer,key}
 
-echo Start compose
+echo :Start compose
 sudo docker-compose up --build
+
