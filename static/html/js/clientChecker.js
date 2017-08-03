e457c4b4c05ac9d0c3e91a884addff257c4a5763
Benjamin Keller <Benjamin.Keller@analytical-software.de>
2017-08-03 15:08:37 +0200

    improved client checker

--- static/html/js/clientChecker.js
+++ static/html/js/clientChecker.js
@@ -1,35 +1,34 @@
 // Race conditions cannot occurs in js.
-this.ClientChecker = function(callback, checkInterval = 5000) {
+this.ClientChecker = function(callback, checkInterval) {
 	var that = this;
-	
-	var clientUrls = [
+
+	that.clientUrls = [
 		"http://127.0.0.1:24727/eID-Client",
 		"eid://127.0.0.1:24727/eID-Client",
 		"http://localhost:24727/eID-Client",
 		"eid://localhost:24727/eID-Client"
 	];
 	var statusParam = "?Status";
-	
-	var activeUrlIndex = -1;
+
 	var clientIsAvailable = false;
-	
+
 	var checkClientStatus = function() {
 		if(!clientIsAvailable) {
-			$.each(clientUrls, function(index, url) {
+			$.each(that.clientUrls, function(index, url) {
 				$.get(url + statusParam)
 					.done(function() {
 						if(!clientIsAvailable) {
 							clientIsAvailable = true;
-							callback(clientUrls[index]);
+							callback(that.clientUrls[index]);
 						}
 					});
 			});
 			setTimeout(checkClientStatus, checkInterval);
 		}
 	};
-	
+
 	// asynchronous function call
 	setTimeout(checkClientStatus, 0);
-	
+
 	return this;
 }
